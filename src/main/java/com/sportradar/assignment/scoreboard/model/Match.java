4e6ca62f4ee41b1fd1318dda1029151073ff6dc2
Martin Bombala <xbombalam@stuba.sk>
2023-05-11 17:24:55 +0200

    Create application model

--- src/main/java/com/sportradar/assignment/scoreboard/model/Match.java
+++ src/main/java/com/sportradar/assignment/scoreboard/model/Match.java
@@ -1,4 +1,13 @@
 package com.sportradar.assignment.scoreboard.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Match {
+
+    private int id;
+    private Team homeTeam;
+    private Team awayTeam;
 }
--- src/main/java/com/sportradar/assignment/scoreboard/model/Team.java
+++ src/main/java/com/sportradar/assignment/scoreboard/model/Team.java
@@ -1,4 +1,16 @@
 package com.sportradar.assignment.scoreboard.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
 public class Team {
+
+    private String name;
+    private int score;
+
+    public Team(String name) {
+        this.name = name;
+        this.score = 0;
+    }
 }
