450c4d6877fed31858f3532f7652c4763c9c9ddd
Martin Bombala <xbombalam@stuba.sk>
2023-05-11 17:40:17 +0200

    Implement null and black check for start match

--- src/main/java/com/sportradar/assignment/scoreboard/Scoreboard.java
+++ src/main/java/com/sportradar/assignment/scoreboard/Scoreboard.java
@@ -2,11 +2,15 @@ package com.sportradar.assignment.scoreboard;
 
 import com.sportradar.assignment.scoreboard.model.Match;
 import org.apache.commons.lang3.NotImplementedException;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
 public class Scoreboard {
     public int startMatch(String homeTeam, String awayTeam) {
+        if (StringUtils.isBlank(homeTeam) || StringUtils.isBlank(awayTeam)) {
+            throw new IllegalArgumentException("home and away team cannot be null nor blank");
+        }
         throw new NotImplementedException("startMatch is not implemented");
     }
 
