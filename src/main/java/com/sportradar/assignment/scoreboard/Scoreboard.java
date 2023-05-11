4152254754e40b94fabcc00e058e0fb81c959bed
Martin Bombala <xbombalam@stuba.sk>
2023-05-11 17:53:42 +0200

    Implement id generator

--- src/main/java/com/sportradar/assignment/scoreboard/Scoreboard.java
+++ src/main/java/com/sportradar/assignment/scoreboard/Scoreboard.java
@@ -5,13 +5,21 @@ import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Scoreboard {
+
+    private final AtomicInteger idGenerator;
+
+    public Scoreboard() {
+        idGenerator = new AtomicInteger(1);
+    }
+
     public int startMatch(String homeTeam, String awayTeam) {
         if (StringUtils.isBlank(homeTeam) || StringUtils.isBlank(awayTeam)) {
             throw new IllegalArgumentException("home and away team cannot be null nor blank");
         }
-        return 1;
+        return idGenerator.getAndIncrement();
     }
 
     public void updateScore(int matchId, int homeTeamScore, int awayTeamScore) {
