c3f2f6f935d7603ee963e9a27076ffa3abeac010
Martin Bombala <xbombalam@stuba.sk>
2023-05-11 17:30:23 +0200

    Add assertions to acceptance test

--- src/test/java/com/sportradar/assignment/scoreboard/ScoreboardAcceptanceTest.java
+++ src/test/java/com/sportradar/assignment/scoreboard/ScoreboardAcceptanceTest.java
@@ -1,6 +1,7 @@
 package com.sportradar.assignment.scoreboard;
 
 import com.sportradar.assignment.scoreboard.model.Match;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -27,5 +28,37 @@ public class ScoreboardAcceptanceTest {
         scoreboard.updateScore(id, 3, 1);
 
         List<Match> summary = scoreboard.getSummary();
+
+        Assertions.assertEquals(5, summary.size());
+
+        Match match = summary.get(0);
+        Assertions.assertEquals("Uruguay", match.getHomeTeam().getName());
+        Assertions.assertEquals(6, match.getHomeTeam().getScore());
+        Assertions.assertEquals("Italy", match.getAwayTeam().getName());
+        Assertions.assertEquals(6, match.getAwayTeam().getScore());
+
+        match = summary.get(1);
+        Assertions.assertEquals("Spain", match.getHomeTeam().getName());
+        Assertions.assertEquals(10, match.getHomeTeam().getScore());
+        Assertions.assertEquals("Brazil", match.getAwayTeam().getName());
+        Assertions.assertEquals(2, match.getAwayTeam().getScore());
+
+        match = summary.get(2);
+        Assertions.assertEquals("Mexico", match.getHomeTeam().getName());
+        Assertions.assertEquals(0, match.getHomeTeam().getScore());
+        Assertions.assertEquals("Canada", match.getAwayTeam().getName());
+        Assertions.assertEquals(5, match.getAwayTeam().getScore());
+
+        match = summary.get(3);
+        Assertions.assertEquals("Argentina", match.getHomeTeam().getName());
+        Assertions.assertEquals(3, match.getHomeTeam().getScore());
+        Assertions.assertEquals("Australia", match.getAwayTeam().getName());
+        Assertions.assertEquals(1, match.getAwayTeam().getScore());
+
+        match = summary.get(4);
+        Assertions.assertEquals("Germany", match.getHomeTeam().getName());
+        Assertions.assertEquals(2, match.getHomeTeam().getScore());
+        Assertions.assertEquals("France", match.getAwayTeam().getName());
+        Assertions.assertEquals(2, match.getAwayTeam().getScore());
     }
 }
